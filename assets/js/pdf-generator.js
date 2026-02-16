1416c7e5481d4e957fafd464be9bbd09474d5808
Lukas Pilz <Lukas.Pilz@iup.uni-heidelberg.de>
2026-02-16 15:53:11 +0100

    Fixed print

--- assets/js/pdf-generator.js
+++ assets/js/pdf-generator.js
@@ -1,5 +1,6 @@
 function print() {
-  const printWindow = window.open("/print", "_blank");
+  const baseurl = window.siteConfig?.baseurl || '';
+  const printWindow = window.open(baseurl + "/print", "_blank");
   printWindow.onload = function () {
     printWindow.print();
     // Close the print window after a delay
@@ -11,7 +12,8 @@ function generatePDF() {
   // Use the browser's native print dialog for best quality
   // JavaScript PDF generators can't match the browser's rendering quality,
   // especially with complex layouts like the continuous sidebar background
-  const printWindow = window.open("/print", "_blank");
+  const baseurl = window.siteConfig?.baseurl || '';
+  const printWindow = window.open(baseurl + "/print", "_blank");
   printWindow.onload = function () {
     printWindow.print();
     // Close the print window after a delay
